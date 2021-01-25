10c80f4dab8d1390d64ff2cad7ba9d72d1b161ca
ogadra <ogaryu0529@gmail.com>
2021-01-26 02:15:58 +0900

    txt file read implemented

--- adverb.py
+++ adverb.py
@@ -1,4 +1,5 @@
 import CaboCha
+import re
 
 def judge(sentence, adverb, attach):
     c = CaboCha.Parser()
@@ -34,3 +35,29 @@ if __name__ == '__main__':
     adverb = "ケッシテ"
     attach = ["ない"]
     print(judge(sentence,adverb,attach))
+
+    corpus = open('kesshite.txt', encoding='utf-8').read().split('\n')
+
+    cntTrue = 0
+    cntFalse = 0
+    for i in range(len(corpus)):
+        corpus[i] = re.sub('　|「|」', '', corpus[i])
+        corpus[i] = corpus[i].split('\t')
+
+        if i >= 1:
+            # corpus[i][3] = re.match(';[^;]+?$',corpus[i][3])
+            corpus[i][3] = corpus[i][3].split(';')[-1]
+            corpus[i][5] = corpus[i][5].split(';')[0]
+
+            # sentence = corpus[i][3:6].join()
+
+            if judge(''.join(corpus[i][3:6]), adverb, attach):
+                cntTrue += 1
+            else:
+                print(''.join(corpus[i][3:6]))
+                cntFalse += 1
+
+
+        if i > 5:
+            break
+    print(cntTrue, cntFalse)
