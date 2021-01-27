a083639850368d3dcaf5ae6e04899cfa504138b6
ogadra <ogaryu0529@gmail.com>
2021-01-27 18:30:20 +0900

    update adverb.py for analysis and prepare csv files

--- adverb.py
+++ adverb.py
@@ -6,7 +6,6 @@ import json
 def judge(sentence, adverb, attach):
     c = CaboCha.Parser()
     tree = c.parse(sentence)
-    # exit()
     chunks = []
     text = ""
     attachIds = []
@@ -39,6 +38,7 @@ def judge(sentence, adverb, attach):
     #     chunk = chunks[j]
     #     if chunk['to'] >= 0:
     #         print(j, chunk['c'] + " →　" + chunks[chunk['to']]['c'])
+    # デバッグ用。係り受け解析結果を表示する。
 
     if looking == -1:
         return None
@@ -59,26 +59,28 @@ def analysis(fileName, adverb, attach, result):
     result.append([adverb[0], 'False'])
     result[-1].extend([0 for i in range(41)])
 
-    NullCnt = 0
-
-    # for i in range(1,len(corpus)-1):
-    for i in range(1,5):
+    for i in range(1,len(corpus)-1):
         corpus[i] = re.sub('　|「|」', '', corpus[i])
         corpus[i] = corpus[i].split('\t')
-        # year == corpus[i][30]
-
         corpus[i][3] = corpus[i][3].split(';')[-1]
         corpus[i][5] = corpus[i][5].split(';')[0]
+        # ファイルから目的の文章を切り取る
 
 
         if fork := judge(''.join(corpus[i][3:6]), adverb, attach=attach):
             result[-2][int(corpus[i][30]) - 1968] += 1
         elif fork == False:
             result[-1][int(corpus[i][30]) - 1968] += 1
-            # print(i,''.join(corpus[i][3:6]))
+            contents = str(i) + ', ' + adverb[0] + ', ' + '|'.join(attach) + ', ' + ''.join(corpus[i][3:6])
+            with open('./exception.csv', mode='a', encoding='utf=8') as f:
+                f.write(contents)
+                f.write('\n')
         elif fork is None:
-            # print(i,''.join(corpus[i][3:6]))
             pass
+        # 判定結果を変数"fork"に代入し、条件式で分岐
+        # 結果がTrue or Falseの場合Resultの該当箇所をインクリメントさせる
+        # Falseの結果をまとめたcsvファイルに文章を追記
+        # 該当する副詞が見つからない場合は何もしない
     return result
 
 
@@ -93,6 +95,6 @@ if __name__ == '__main__':
         result = analysis(i['fileName'], i['adverb'], i['attach'], result)
 
     result = [list(x) for x in zip(*result)]
-    with open('result.csv', 'w', newline='') as f:
+    with open('result.csv', 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerows(result)
